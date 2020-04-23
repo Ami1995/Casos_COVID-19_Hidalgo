6c42d286fb73aa89e48855aa5d88507291311994
Ami1995 <55511173+Ami1995@users.noreply.github.com>
2020-04-23 00:57:40 -0500

    Update Hidalgo2204.R

--- 02_codigo/Hidalgo2204.R
+++ 02_codigo/Hidalgo2204.R
@@ -24,9 +24,11 @@ covid_22_04 <- read_csv("01_datos/22 abril /200422COVID19MEXICO.csv")
 
 # Municipios de Hgo 
 
-catalogo_mun <- read_excel("01_datos/diccionario_datos_covid19/Catalogos_0412.xlsx", 
-                           sheet = "Catálogo MUNICIPIOS", range = "A466:C550", col_names = c("clave_municipio", "nombre_municipio", "clave_entidad"))
-#View(catalogo_mun)
+catalogo_mun <- read_excel("01_datos/13abril2020/diccionario_datos_covid19/Catalogos_0412.xlsx", 
+                             sheet = "Catálogo MUNICIPIOS", range = "A466:C550", 
+                           col_names = c("clave_municipio", "nombre_municipio", "clave_entidad"))
+
+View(catalogo_mun)
 
 # Archivo geojson municipios Hgo
 
